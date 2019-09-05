7d38072af7fef5a67461d7a2f31860ef63dcf56d
dreamdameng <dreamdameng@163.com>
2019-09-05 16:37:48 +0800

    第一次push

---
+++ app/src/main/java/com/dreamdameng/unatwo/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.dreamdameng.unatwo.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yangmeng on 2019/8/29.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
